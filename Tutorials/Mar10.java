826fd4fd052f239452689d39e5e95ddc809f8d82
danijourdain <danijourdain@icloud.com>
2022-03-10 20:48:06 +0000

    adding March 10 tutorial file

---
+++ Tutorials/Mar10.java
@@ -0,0 +1,168 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Mar10 {
+    public static void main(String[] args) {
+        BSTree bst = new BSTree();
+
+        int[] array = {6, 2, 8, 1, 4, 3};
+
+        for(int a: array) {
+            bst.insert(a);
+        }
+
+        bst.remove(2);
+        bst.printTree();
+
+        System.out.println("min is " + bst.findMinRecursive());
+        System.out.println("max is " + bst.findMaxRecursive());
+        System.out.println("height is " + bst.findHeight());
+    }
+
+    public static void readFile() {
+        try(BufferedReader br = new BufferedReader(new FileReader("a3input1.txt"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                char[] studentNumber = new char[7];
+                line.getChars(1, 8, studentNumber, 0);
+                System.out.println(new String(studentNumber));
+            }
+        }
+        catch(Exception e) {
+            System.out.println("error");
+        }
+    }
+}
+
+class BSTree {
+    private static class BSNode {
+        int data;
+        BSNode left, right;
+
+        BSNode(int d, BSNode l, BSNode r) {
+            data = d;
+            left = l;
+            right = r;
+        }
+    }
+
+    BSNode root;
+
+    BSTree() {
+        root = null;
+    }
+
+    void insert(int d) {
+        root = insert(d, root);
+    }
+
+    BSNode insert(int d, BSNode t) { // t is root of curent subtree
+        if(t == null) {
+            return new BSNode(d, null, null);
+        }
+        else if (d < t.data) {
+            t.left = insert(d, t.left);
+        }
+        else if (d > t.data) {
+            t.right = insert(d, t.right);
+        }
+        return t;
+    }
+
+    void remove(int d) {
+        root = remove(d, root);
+    }
+
+    BSNode remove(int d, BSNode t) {
+        if(t == null) {
+            //not found
+            return t;
+        }
+        if(d < t.data) {
+            t.left = remove(d, t.left);
+        }
+        else if(d > t.data) {
+            t.right = remove(d, t.left);
+        }
+        else if(t.left != null && t.right != null){
+            //step 1:find min node on right subtree
+            //step 2: copy min node to t, and delete min node from right subtree
+            
+            t.data = findMin(t.right).data;
+            remove(t.data, t.right);
+
+
+        }
+        else { //t has < 2 children
+            return (t.left == null) ? t.right : t.left;
+        }
+        return t;
+    }
+
+    BSNode findMin(BSNode t) {
+        if(t == null) { // no min found
+            return t;
+        }
+        else if(t.left == null) {
+            return t;
+        }
+        return findMin(t.left);
+    }
+
+    boolean contains(int d, BSNode t) {
+        if(t == null) {
+            return false;
+        }
+        if(d < t.data) {
+            return contains(d, t.left);
+        }
+        else if (d > t.data) {
+            return contains(d, t.right); 
+        }
+        else {
+            return true;
+        }
+    }
+
+    void printTree() {
+        printTree(root);
+    }
+
+    void printTree(BSNode t) {
+        if(t != null) {
+            printTree(t.left);
+            System.out.println(t.data);
+            printTree(t.right);
+        }
+    }
+
+    int findMinRecursive() {
+        return findMin(root).data;
+    }
+
+    int findMaxRecursive() {
+        return findMax(root).data;
+    }
+
+    BSNode findMax(BSNode current) {
+        if(current.right == null) {
+            return current;
+        }
+        else {
+            return findMax(current.right);
+        }
+    }
+
+    int findHeight() {
+        return findHeight(root);
+    }
+
+    int findHeight(BSNode t) {
+        if(t == null) {
+            return 0;
+        }
+        return Math.max(findHeight(t.left), findHeight(t.right)) + 1;
+    }
+
+    
+}
